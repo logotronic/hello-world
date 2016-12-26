ae5e36ba858a21d6be3c74b3209cdfa1e81ab18d
Logan Boespflug <logboespflug@gmail.com>
2016-12-26 15:28:35 -0700

    Add files via upload

---
+++ C/Item23Drill/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item23_number1
+{
+    interface Interface
+    {
+    }
+}
