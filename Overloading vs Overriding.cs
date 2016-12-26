4a9fa1b396f3f4865922910cab0ec255068c79ec
Logan Boespflug <logboespflug@gmail.com>
2016-12-26 15:14:53 -0700

    Add files via upload

---
+++ Overloading vs Overriding.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item23_number1
+{
+    class Snowing : Weather
+    {
+        public string hightoday { get; set; }
+        public Snowing() : base()
+        {
+            this.hightoday = "Lower than H2O freezing point";
+        }
+
+        public Snowing(double wind, double inperhour, string windchill)
+            base(wind,inperhour,windchill)
+        {
+            this.hightoday=hightoday;
+        }
+        new public string toString()
+        {
+            return String.Format("Wind today is {0} per hour, snow rate at {1} inch per hour and really feels like {2}", wind, inperhour, windchill);
+        }
+        
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
