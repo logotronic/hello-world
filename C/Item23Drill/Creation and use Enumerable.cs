dcbcc2a40441f5be0726e8e0330328c6dacb1efa
Logan Boespflug <logboespflug@gmail.com>
2016-12-27 14:25:51 -0800

    Add files via upload

---
+++ C/Item23Drill/Creation and use Enumerable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item23_number1
+{
+    class Creation_and_use_Enumerable
+    {
+        // ---------- ENUMS ----------
+        // Enums are unique types with symbolic names and associated values
+
+        public enum Temperature
+        {
+            Freeze,
+            Low,
+            Warm,
+            Boil
+        }
+    }
+}
---
+++ C/Item23Drill/Creation of a struct.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item23_number1
+{
+    class Creation_of_a_struct
+    {
+        // ---------- STRUCT ----------
+        // A struct is a custom type that holds data made up from different data types
+        struct Customers
+        {
+            private string name;
+            private double balance;
+            private int id;
+
+            public void createCust(string n, double b, int i)
+            {
+                name = n;
+                balance = b;
+                id = i;
+            }
+
+            public void showCust()
+            {
+                Console.WriteLine("Name : " + name);
+                Console.WriteLine("Balance : " + balance);
+                Console.WriteLine("ID : " + id);
+            }
+        }
+    }
+}
