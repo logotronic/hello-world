0c63e2654371da2729a18d195b0338e20e05be7d
Logan Boespflug <logboespflug@gmail.com>
2016-12-27 15:04:37 -0800

    Add files via upload

---
+++ C/Item23Drill/Creation and use of delegate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Item23_number1
+{
+    class Creation_and_use_of_delegate
+    {
+        // Delegates are used to pass methods as arguments to other methods
+        // A delegate can represent a method with a similar return type and attribute list
+        delegate double GetSum(double num1, double num2);
+    }
+}
---
+++ C/Item23Drill/Difference between abstract and interface.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractsANDInterfaces
+    {
+        /// 
+        /// Summary description for Employee.
+        public abstract class Employee
+        {
+            //we can have fields and properties 
+            //in the Abstract class
+            protected String id;
+            protected String lname;
+            protected String fname;
+            //properties
+           
+            public abstract String ID
+            {
+                get;
+                set;
+            }
+
+            public abstract String FirstName
+            {
+                get;
+                set;
+            }
+
+            public abstract String LastName
+            {
+                get;
+                set;
+            }
+            //completed methods
+
+            public String Update()
+            {
+                return "Employee " + id + " " +
+                          lname + " " + fname +
+                          " updated";
+            }
+            //completed methods
+
+
+            public String Add()
+            {
+                return "Employee " + id + " " +
+                          lname + " " + fname +
+                          " added";
+            }
+            //completed methods
+
+
+            public String Delete()
+            {
+                return "Employee " + id + " " +
+                          lname + " " + fname +
+                          " deleted";
+            }
+            //completed methods
+
+
+            public String Search()
+            {
+                return "Employee " + id + " " +
+                          lname + " " + fname +
+                          " found";
+            }
+
+
+            public abstract String CalculateWage();
+
+        }
+    }
+
+namespace AbstractsANDInterfaces
+    {
+        /// <span class="code-SummaryComment"><summary></span>
+
+
+        /// Summary description for IEmployee.
+
+        /// <span class="code-SummaryComment"></summary></span>
+
+        public interface IEmployee
+        {
+            //cannot have fields. uncommenting 
+
+
+            //will raise error!
+            //        protected String id;
+            //        protected String lname;
+            //        protected String fname;
+
+
+            //just signature of the properties 
+
+            //and methods.
+
+            //setting a rule or contract to be 
+
+            //followed by implementations.
+
+
+            String ID
+            {
+                get;
+                set;
+            }
+
+            String FirstName
+            {
+                get;
+                set;
+            }
+
+            String LastName
+            {
+                get;
+                set;
+            }
+
+            // cannot have implementation
+
+
+            // cannot have modifiers public 
+
+            // etc all are assumed public
+
+            // cannot have virtual
+
+
+            String Update();
+
+            String Add();
+
+            String Delete();
+
+            String Search();
+
+            String CalculateWage();
+        }
+    }
+}
+}
